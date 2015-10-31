f1361b608cb42bc2ab9384201aacbb75417cd186
Saumya Dwivedi <saumya.zero@gmail.com>
2015-10-31 19:55:55 -0400

    TD-6 User service added

--- app.js
+++ app.js
@@ -8,9 +8,13 @@ app.use(bodyParser.json());
 var todoService = require('./services/todo.js');
 todoService.LinkApp(app);
 
+// Include the Users service and let it bind to the application
+var userService = require('./services/user.js');
+userService.LinkApp(app);
+
 var server = app.listen(8080, function() {
 	var host = server.address().address;
 	var port = server.address().port;
 
-	console.log('Direct your browser to http://%s:%s/api/todo-lists', host, port);
+	console.log('Direct your browser to http://%s:%s/api/users', host, port);
 });
